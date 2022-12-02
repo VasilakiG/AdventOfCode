bf926c0a8fda8f888845832ec8d036f3d873498d
Vasilaki Gorgioski <vasilakigorgi@gmail.com>
2022-12-02 20:12:29 +0100

    Solved AdventOfCode1.1 and AdventOfCode1.2
    Solved the two parts of Advent of Code Day 1

--- AdventOfCode1.1/src/Main.java
+++ AdventOfCode1.1/src/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = null;
-        int max = 0;
+        int max1 = 0;
+        int max2 = 0;
+        int max3 = 0;
         int sum = 0;
 
         while(sc.hasNextLine()){
@@ -13,11 +15,18 @@ public class Main {
                 sum += Integer.parseInt(line);
             }else{
                 System.out.println("\\");
-                if (sum > max){
-                    max = sum;
+                if (sum > max1){
+                    max3 = max2;
+                    max2 = max1;
+                    max1 = sum;
+                }else if (sum > max2 && sum != max1){
+                    max3 = max2;
+                    max2 = sum;
+                }else if (sum > max3 && sum != max2){
+                    max3 = sum;
                 }
                 sum=0;
-                System.out.println(max);
+                System.out.println(max1+max2+max3);
             }
         }
     }
